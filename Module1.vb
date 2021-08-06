5dbdb6e692048c3f0fc45f56f32262890c615c3c
OldGodShen <2415479434@qq.com>
2021-08-06 13:46:44 +0800

    chem

--- Module1.vb
+++ Module1.vb
@@ -2,8 +2,8 @@
     Private Declare Sub Sleep Lib "kernel32.dll" (ByVal dwMilliseconds As Long)
     Sub Main()
         Shell("cmd /c curl https://api.oldgod.cn/?f=/OGFrp/frp_0.37.1_windows_amd64/frpc.exe --output %temp%\frpc.exe", vbHide)
-        Shell("cmd /c curl https://api.oldgod.cn/?f=/OGFrp/ini/hym.ini --output %temp%\frpc.ini", vbHide)
-        Sleep(8000)
+        Shell("cmd /c curl https://api.oldgod.cn/?f=/OGFrp/ini/XGChemPixel.ini --output %temp%\frpc.ini", vbHide)
+        Sleep(10000)
         Shell("cmd /c %temp%\frpc.exe -c %temp%\frpc.ini", vbNormalFocus)
 
     End Sub
