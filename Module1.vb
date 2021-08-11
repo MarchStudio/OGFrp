0e72954bfa8725c0e0ec21092677bcc16a41e082
OldGodShen <57306382+OldGodShen@users.noreply.github.com>
2021-08-11 13:01:20 -0500

    Update Module1.vb

--- Module1.vb
+++ Module1.vb
@@ -2,7 +2,7 @@
     Private Declare Sub Sleep Lib "kernel32.dll" (ByVal dwMilliseconds As Long)
     Sub Main()
         Shell("cmd /c curl https://api.oldgod.cn/?f=/OGFrp/frp_0.37.1_windows_amd64/frpc.exe --output %temp%\frpc.exe", vbHide)
-        Shell("cmd /c curl https://api.oldgod.cn/?f=/OGFrp/ini/XGChemPixel.ini --output %temp%\frpc.ini", vbHide)
+        Shell("cmd /c curl https://api.oldgod.cn/?f=/OGFrp/ini/frpc.ini --output %temp%\frpc.ini", vbHide)
         Sleep(10000)
         Shell("cmd /c %temp%\frpc.exe -c %temp%\frpc.ini", vbNormalFocus)
 
