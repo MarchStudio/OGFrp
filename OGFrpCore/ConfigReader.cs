f10c2a1a2ba4732d7b04237fab44d9459a2481cd
黄宇孟 <hym2007@outlook.com>
2021-10-01 02:31:14 +0800

    GUI-Update-211001-02

--- OGFrpCore/ConfigReader.cs
+++ OGFrpCore/ConfigReader.cs
@@ -10,14 +10,45 @@ namespace OGFrpCore
     class Config
     {
 
+        public Config()
+        {
+            ReadConfig();
+        }
+
+        /// <summary>
+        /// config.ini的位置
+        /// </summary>
         private string configpath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\OGFrp\\config.ini";
 
+        /// <summary>
+        /// 重新载入配置文件
+        /// </summary>
+        /// <returns>0表示成功，-1表示有异常发生</returns>
+        public int ReadConfig()
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(configpath);
+            }
+            catch
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+
+        /// <summary>
+        /// 配置信息
+        /// </summary>
         public struct config
         {
             /// <summary>
             /// 设置的语言
             /// </summary>
             public string lang { get; set; }
+        }
+
 
 
         public string GetLang()
