897ebcfde13e740060afc07e916e29589d4768ab
GreatHuang2007 <hym2007@outlook.com>
2021-11-07 10:40:15 +0800

    FrpServer-List-Update-211107

---
+++ src/OGFrp.SharedCS/FrpServer/FrpServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace OGFrp.UI
+{
+    public class FrpServer
+    {
+        /// <summary>
+        /// 将api.ogfrp.cn上的节点信息转换成FrpServerModel
+        /// </summary>
+        /// <param name="Content">将api.ogfrp.cn上的节点信息</param>
+        public FrpServerModel StringToServer(string Content)
+        {
+            FrpServerModel result = new FrpServerModel();
+
+            var reader = new StreamReader(Content);
+
+            return result;
+        }
+    }
+}
---
+++ src/OGFrp.SharedCS/FrpServer/FrpServerModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OGFrp.UI
+{
+    public class FrpServerModel
+    {
+        /// <summary>
+        /// 节点ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 节点名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 节点描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 节点IP
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
