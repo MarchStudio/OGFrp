68470e3908b7b81500bb3c1072066efce31e30b3
GreatHuang2007 <hym2007@outlook.com>
2022-03-21 11:14:54 +0800

    Fix the include<string.h> mistake in OGFrp.Unix.

--- src/OGFrp.Unix.Common/libs.cpp
+++ src/OGFrp.Unix.Common/libs.cpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <string.h>
 #include <sys/statfs.h>
 #include <limits.h>
 #include <unistd.h>
 #include <iostream>
+#include <string>
 
 /// get executable path
 std::string get_cur_executable_path_() {
