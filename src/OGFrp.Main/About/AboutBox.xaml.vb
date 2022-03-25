1c39bcbbfa69e7d0fb5bc33aa3233099ad52e345
GreatHuang2007 <hym2007@outlook.com>
2022-03-25 10:28:28 +0800

    Add repo url link in about page.

--- src/OGFrp.Main/About/AboutBox.xaml.vb
+++ src/OGFrp.Main/About/AboutBox.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Public Class AboutBox
-
+    Private Sub Lb_OpenSource_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Lb_OpenSource.MouseDown
+        Process.Start("https://github.com/MarchStudio/OGFrp")
+    End Sub
 End Class
